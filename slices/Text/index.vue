2d4c2bae02d6c7fa005abfc37b2474383bb5053d
Louise Findlay <louise.findlay@prismic.io>
2023-05-31 11:51:00 +0100

    Templated Text slice

---
+++ slices/Text/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
+</script>
+
+<template>
+  <prismic-text :field="slice.primary.text"></prismic-text>
+</template>
