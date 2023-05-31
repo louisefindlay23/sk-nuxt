86eff51d4214452e7fa035eb10e7a0e6355116da
Louise Findlay <louise.findlay@prismic.io>
2023-05-31 12:26:53 +0100

    Installed prettier-no-newline

---
+++ components/Footer/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import styles from "./Footer.module.css";
+const siteTitle = "The Sock Kingdom";
+</script>
+
+<template>
+  <footer :class="styles.pageFooter">
+    <p>© {{ new Date().getFullYear() }} {{ siteTitle }}</p>
+  </footer>
+</template>
---
+++ components/Header/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import styles from "./Header.module.css";
+const siteTitle = "The Sock Kingdom";
+</script>
+
+<template>
+  <header :class="styles.pageHeader">
+    <a href="/">
+      <h1>{{ siteTitle }}</h1>
+    </a>
+  </header>
+</template>
