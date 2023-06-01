300318f3849fcd1c8cf6f14e0431f4e2f25119b7
Louise Findlay <louise.findlay@prismic.io>
2023-06-01 12:43:54 +0100

    Added Google Fonts and prism syntax highlighting with line breaks

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <script setup>
 import "./styles/main.css";
+useHead({
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Inter&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Arvo:wght@700&display=swap",
+    },
+  ],
+});
 </script>
 
 <template>
--- slices/Code/index.vue
+++ slices/Code/index.vue
@@ -1,11 +1,25 @@
 <script setup>
 import Prism from "prismjs";
+import "./prism-ghcolors.css";
+
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Fira+Code&display=swap",
+    },
+  ],
+});
 
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
 
+Prism.hooks.add("before-highlight", function (env) {
+  env.code = env.element.innerText;
+});
+
 const codeSerializer = {
   preformatted: ({ children }) =>
-    `<pre><code class="language-html">${children}</pre>`,
+    `<pre><code class="language-html">${children}</code></pre>`,
 };
 </script>
 
