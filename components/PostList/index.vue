8522f29f317a66c7d0c9aad7546e883907450e0d
Louise Findlay <louise.findlay@prismic.io>
2023-06-01 15:27:07 +0100

    Added basic pagination

--- components/PostList/index.vue
+++ components/PostList/index.vue
@@ -5,6 +5,22 @@ const { client } = usePrismic();
 const { data: posts } = await useAsyncData("posts", () =>
   client.getAllByType("posts")
 );
+
+// Pagination functions
+const defaultPosts = posts.slice(0, 1);
+const postPage = 1;
+
+const getPreviousPosts = () => {
+  const previousPosts = posts.slice(postPage - 1, postPage);
+  const previousPage = postPage - 1;
+};
+
+const getNextPosts = () => {
+  const nextPosts = posts.slice(postPage + 1, postPage + 2);
+  const nextPage = postPage + 1;
+};
+
+console.info(posts);
 </script>
 
 <template>
@@ -14,10 +30,20 @@ const { data: posts } = await useAsyncData("posts", () =>
         <prismic-rich-text :field="post.data.body[0].primary.heading" />
       </prismic-link>
       <Date :postDate="post.first_publication_date" />
+      <!-- Slice the post's first paragraph for the excerpt -->
       <prismic-rich-text :field="post.data.body[2].primary.text.slice(0, 1)" />
     </div>
     <div :class="styles.boxImage">
       <prismic-image :field="post.data.body[1].primary.image" />
     </div>
   </article>
+  <!-- Pagination buttons -->
+  <div :class="styles.pagination">
+    <button @click="getPreviousPosts" :disabled="postPage === 1">
+      Previous
+    </button>
+    <button @click="nextPage" :disabled="postPage === posts.length - 1">
+      Next
+    </button>
+  </div>
 </template>
