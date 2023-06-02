4f807c06108632ea2b60618fd699d3949b891d8d
Louise Findlay <louise.findlay@prismic.io>
2023-06-02 17:08:26 +0100

    Updated navigation to switch locales

--- components/Header/Navigation/Navigation.vue
+++ components/Header/Navigation/Navigation.vue
@@ -5,15 +5,20 @@ const { client } = usePrismic();
 const { data: navigation } = await useAsyncData("navigation", (locale) =>
   client.getSingle("navigation", { lang: locale.value })
 );
+const nuxtApp = useNuxtApp();
+const locale = nuxtApp.$i18n.locale;
 </script>
 
 <template>
   <nav v-if="navigation">
     <ul :class="styles.navList">
       <li v-for="link in navigation.data.menu" :key="JSON.stringify(link)">
-        <prismic-link :field="link.link_url">
+        <!-- <prismic-link :field="link.link_url">
           {{ prismicH.asText(link.link_text) }}
-        </prismic-link>
+        </prismic-link> -->
+        <nuxt-link :to="`/${locale}${link.link_url.url}`">
+          {{ prismicH.asText(link.link_text) }}
+        </nuxt-link>
       </li>
     </ul>
     <LanguageSwitcher />
