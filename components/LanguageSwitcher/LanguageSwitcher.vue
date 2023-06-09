290b93f8b0cb4390f1f8b8bac101126678798ea3
Louise Findlay <louise.findlay@prismic.io>
2023-06-09 09:55:12 +0100

    Fixed dropdown console error

--- components/LanguageSwitcher/LanguageSwitcher.vue
+++ components/LanguageSwitcher/LanguageSwitcher.vue
@@ -2,29 +2,42 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import styles from "./LanguageSwitcher.module.css";
 
+const nuxtApp = useNuxtApp();
 const { locale } = useI18n();
 
-const getLocales = useState("locales");
+const locales = ref(null);
+const currentLocale = ref(null);
 
-// Find lang name from current locale to use in language selector display value
-const getCurrentLocaleName = computed(() => {
-  const locales = getLocales.value;
-  return locales ? locales.find((loc) => loc.lang === locale.value) : undefined;
-});
+const fetchLocales = async () => {
+  const getLocales = useState("locales");
+  return await getLocales.value;
+};
 
 const switchLocale = (value) => {
   value = value.value;
   nuxtApp.$i18n.setLocale(value.lang);
+  currentLocale.value = value;
   navigateTo(value.url);
 };
+
+onMounted(async () => {
+  locales.value = await fetchLocales();
+  // Find lang name from current locale to use in language selector display value
+  currentLocale.value = locales.value.find((loc) => loc.lang === locale.value);
+});
+
+function getFlagName(flagName) {
+  flagName = flagName.toString();
+  return flagName.substring(3);
+}
 </script>
 
-// PrimeVue dropdown component to show country flags
+<!-- PrimeVue dropdown component to show country flags -->
 <template>
-  <div :class="styles.languageSelect">
+  <div v-if="locales" :class="styles.languageSelect">
     <Dropdown
       v-model="locale"
-      :options="getLocales"
+      :options="locales"
       @change="switchLocale"
       optionLabel="lang_name"
       placeholder="Select a Country"
@@ -36,14 +49,14 @@ const switchLocale = (value) => {
       <template #value="slotProps">
         <div v-if="slotProps.value" class="flex align-items-center">
           <img
-            :alt="slotProps.value.label"
+            :alt="currentLocale.lang_name"
             src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-            :class="`mr-2 fi fi-${slotProps.value.substring(3)} ${
+            :class="`mr-2 fi fi-${getFlagName(slotProps.value)} ${
               styles.dropdownDisplay
             } ${styles.dropdownImage}`"
           />
-          <div v-if="getCurrentLocaleName" :class="styles.dropdownDisplay">
-            {{ getCurrentLocaleName.lang_name }}
+          <div v-if="currentLocale" :class="styles.dropdownDisplay">
+            {{ currentLocale.lang_name }}
           </div>
         </div>
         <span v-else>
