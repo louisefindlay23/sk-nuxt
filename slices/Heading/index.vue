fea16c2a4ef7a1930ba27fe362010e7eb0ef4ec6
Louise Findlay <louise@louisefindlay.com>
2024-03-07 21:15:55 +0000

    Removed unneeded packages

--- slices/Heading/index.vue
+++ slices/Heading/index.vue
@@ -1,17 +1,7 @@
 <script setup>
-import pigLatin from "piglatin";
-
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
-
-// Convert h2s into Pig Latin
-const latinSerializer = {
-  heading4: ({ children }) => `<h4>${pigLatin(children)}</h4>`,
-};
 </script>
 
 <template>
-  <prismic-rich-text
-    :field="slice.primary.heading"
-    :htmlSerializer="latinSerializer"
-  ></prismic-rich-text>
+  <prismic-rich-text :field="slice.primary.heading"></prismic-rich-text>
 </template>
--- slices/index.js
+++ slices/index.js
@@ -1,7 +1,6 @@
-import code from "./Code";
 import heading from "./Heading";
 import image from "./Image";
 import recent_posts from "./RecentPosts";
 import text from "./Text";
 
-export { code, heading, image, recent_posts, text };
+export { heading, image, recent_posts, text };
